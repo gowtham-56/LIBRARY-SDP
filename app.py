b01666d3fe0d539e7967fe2d35c733fdc14c93b9
Gowtham S P <167736197+gowtham-56@users.noreply.github.com>
2024-11-09 09:20:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def delete_book(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Creates the database with the Book table
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0',port=5000)
